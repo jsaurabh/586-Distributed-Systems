8165313afd5c5660b89ea1e3f498390e74143faf
Saurabh <j.saur95@gmail.com>
2019-03-25 14:57:09 -0400

    checkpoint

--- GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerProvider.java
+++ GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerProvider.java
@@ -2,29 +2,43 @@ package edu.buffalo.cse.cse486586.groupmessenger2;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
 import android.util.Log;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * GroupMessengerProvider is a key-value table. Once again, please note that we do not implement
  * full support for SQL as a usual ContentProvider does. We re-purpose ContentProvider's interface
  * to use it as a key-value table.
- * 
+ *
  * Please read:
- * 
+ *
  * http://developer.android.com/guide/topics/providers/content-providers.html
  * http://developer.android.com/reference/android/content/ContentProvider.html
- * 
+ *
  * before you start to get yourself familiarized with ContentProvider.
- * 
+ *
  * There are two methods you need to implement---insert() and query(). Others are optional and
  * will not be tested.
- * 
+ *
  * @author stevko
  *
  */
 public class GroupMessengerProvider extends ContentProvider {
+
+    private static final String PROVIDER_TAG = GroupMessengerProvider.class.getSimpleName();
+    private static final String mKEY = "key";
+    private static final String mVALUE = "value";
+    //Context context = getContext();
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -39,24 +53,52 @@ public class GroupMessengerProvider extends ContentProvider {
     }
 
     @Override
-    public Uri insert(Uri uri, ContentValues values) {
+    public Uri insert(Uri uri, ContentValues mValues) {
         /*
          * TODO: You need to implement this method. Note that values will have two columns (a key
          * column and a value column) and one row that contains the actual (key, value) pair to be
          * inserted.
-         * 
+         *
          * For actual storage, you can use any option. If you know how to use SQL, then you can use
          * SQLite. But this is not a requirement. You can use other storage options, such as the
          * internal storage option that we used in PA1. If you want to use that option, please
          * take a look at the code for PA1.
          */
-        Log.v("insert", values.toString());
+
+        String mName;
+        String mFileString;
+        FileOutputStream mFile;
+
+        mName = mValues.getAsString(mKEY);
+        mFileString = mValues.getAsString(mVALUE);
+
+        //Modified from PA1 file storage snippet
+        try {
+            /*https://developer.android.com/reference/android/content/Context.html#openFileOutput(java.lang.String,%20int)
+             * Alternatively, can pass current activity's context using ContentValues.put() from main activity
+             */
+            //mFile = context.openFileOutput(mName, Context.MODE_PRIVATE);
+            //openFileOutput will create a new file if doesn't exist. Else, rewrites
+            mFile = getContext().openFileOutput(mName, Context.MODE_PRIVATE);
+            mFile.write(mFileString.getBytes());
+            mFile.flush(); //empty method declaration
+            mFile.close();
+
+        } catch (IOException e) {
+            Log.d(PROVIDER_TAG, "IO Exception: insert method");
+        } catch (NullPointerException e){
+            Log.d(PROVIDER_TAG, "Null Pointer Exception: insert method");
+        } catch (Exception e){
+            e.printStackTrace();
+            Log.d(PROVIDER_TAG, "File write failed");
+        }
         return uri;
     }
 
     @Override
     public boolean onCreate() {
         // If you need to perform any one-time initialization task, please do it here.
+        //Context context = this.getContext();
         return false;
     }
 
@@ -80,7 +122,31 @@ public class GroupMessengerProvider extends ContentProvider {
          * recommend building a MatrixCursor described at:
          * http://developer.android.com/reference/android/database/MatrixCursor.html
          */
-        Log.v("query", selection);
-        return null;
+        FileInputStream mFile;
+        String mString;
+        StringBuffer mStringBuffer;
+        BufferedReader mReaderIn;
+        String mFileString;
+
+        MatrixCursor mMatrixCursor = new MatrixCursor(new String[]{mKEY, mVALUE});
+        try{
+            mFile = getContext().openFileInput(selection);
+            mStringBuffer = new StringBuffer();
+            mReaderIn = new BufferedReader(new InputStreamReader(mFile));
+
+            while ((mString = mReaderIn.readLine()) != null){
+                mStringBuffer.append(mString);
+            }
+            mFileString = mStringBuffer.substring(0);
+            synchronized (this){
+                mMatrixCursor.addRow(new String[]{selection, mFileString});
+            }
+            mFile.close();
+        }catch (FileNotFoundException e){
+            Log.d(PROVIDER_TAG, "File Not Found");
+        }catch (IOException e){
+            Log.d(PROVIDER_TAG, "IO Exception occurred. Query method");
+        }
+        return mMatrixCursor;
     }
 }
